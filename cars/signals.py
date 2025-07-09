e05185c0ab53d20109ad38c12aaad56997ba7bb8
Anderson Nascimento <and.pax@gmail.com>
2025-07-09 20:46:35 -0300

    Integração com o DeepSeek

--- cars/signals.py
+++ cars/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
+from openai_api.client import get_car_ia_bio
 
 
 def car_inventory_update():
@@ -14,6 +15,13 @@ def car_inventory_update():
         cars_count = cars_count,
         cars_value = cars_value
     )
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    ai_bio = get_car_ia_bio(instance.model, instance.brand, instance.model_year)
+    instance.bio = ai_bio
+
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
